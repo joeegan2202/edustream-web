9d099ae821e3fb656a56bbae3764f9ad39a40c78
Joe Egan <joeegan2202@gmail.com>
2020-06-21 22:14:01 -0400

    Basic routing is working. Ready to start actually building

--- src/App.js
+++ src/App.js
@@ -1,38 +1,26 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
-import logo from './logo.svg'
 import './App.css'
 
 import Auth from './Auth'
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-        </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-    <Link to="/">Go home!</Link>
-            <Link to="/auth">Go to Auth!</Link>
-          </a>
-        </header>
-        <Switch>
-          <Route path="/auth">
-            <Auth />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  )
+    return (
+        <Router>
+            <div className="App">
+                <header className="App-header">
+                    <Link to="/">Go home!</Link>
+                    <Link to="/auth">Go to Auth!</Link>
+                </header>
+
+                <Switch>
+                    <Route path="/auth">
+                        <Auth />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    )
 }
 
 export default App
--- src/Auth.js
+++ src/Auth.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, withRouter } from "react-router-dom"
+import { BrowserRouter as Router, Switch, Route, Link, withRouter } from "react-router-dom"
 
 class Auth extends React.Component {
   constructor(props) {
@@ -13,34 +13,25 @@ class Auth extends React.Component {
   render() {
     console.log(this.props.match.url)
     return (
-      <div className="Auth">
-        <div id="login-card">
-          <Link to={`${this.props.match.url}/logged-in`}><button>Login with google!</button></Link>
-        </div>
-      </div >
+      <Router>
+        <div className="Auth">
+          <Switch>
+            <Route exact path={`${this.props.match.url}`}>
+              <div id="login-card">
+                <Link to={`${this.props.match.url}/logged-in`}><button>Login with google!</button></Link>
+              </div>
+            </Route>
+            <Route exact path={`${this.props.match.url}/logged-in`}>
+              <h1>Logged in!</h1>
+              <Link to={`${this.props.match.url}`}>
+                <h1>Go back to normal auth!</h1>
+              </Link>
+            </Route>
+          </Switch>
+        </div >
+      </Router >
     )
   }
 }
 
-class AuthLoggedIn extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-
-    }
-  }
-
-  render() {
-    return (
-      <div className="Auth">
-
-      </div>
-    )
-  }
-}
-
-let routedAuth = withRouter(Auth)
-let routedAuthLoggedIn = withRouter(AuthLoggedIn)
-
-export { routedAuth as Auth, routedAuthLoggedIn as AuthLoggedIn }
+export default withRouter(Auth)
