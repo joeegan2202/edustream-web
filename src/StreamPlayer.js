80c3439f49336b9af0807246d24c0142b10177ee
Joe Egan <joeegan2202@gmail.com>
2020-06-23 16:29:15 -0400

    Built more into StreamPlayer

--- src/StreamPlayer.js
+++ src/StreamPlayer.js
@@ -1,16 +1,26 @@
 import React from 'react'
 import Hls from 'hls.js'
+import Alert from 'react-bootstrap/Alert'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 class StreamPlayer extends React.Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      source: "http://freja.hiof.no:1935/rtplive/_definst_/hessdalen03.stream/playlist.m3u8"
+      session: 'f864a3bd5a4b48394ba49636f93f19a23fdb35da38ad66bd490f0c9e79a58db2',
+      source: 'http://18.222.231.117:8080/stream/f864a3bd5a4b48394ba49636f93f19a23fdb35da38ad66bd490f0c9e79a58db2/stream.m3u8',
+      errors: []
     }
   }
 
-  load() {
+  componentDidMount() {
+    fetch(`http://18.222.231.117:8080/request/?sid=fda734d93365f6ac6ced0f3d0c85aad460e1a8fc317c998c15546f6ab3d56f73&session=${this.state.session}`).then(data => data.json()).then(output => {
+      if (!output.status) {
+        this.state.errors.push({ title: "Bad Session Request!", message: "Failure while fetching to request session!" })
+        this.setState({})
+      }
+    })
     var video = document.querySelector('video');
     if (Hls.isSupported()) {
       var hls = new Hls();
@@ -37,6 +47,12 @@ class StreamPlayer extends React.Component {
   render() {
     return (
       <div className="StreamPlayer">
+        {this.state.errors.map((error, id) => (
+          <Alert key={id} variant="warning">
+            <Alert.Heading>{error.title}</Alert.Heading>
+            {error.message}
+          </Alert>
+        ))}
         <video></video>
       </div>
     )
